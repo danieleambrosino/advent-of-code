4bfabf82f3131692fe92eae560717d6047e6986a
Daniele Ambrosino <mail@danieleambrosino.it>
2022-12-05 16:43:00 +0100

    refactor(day-05.py): minor improvements

--- 2022/day-05/day-05.py
+++ 2022/day-05/day-05.py
@@ -1,16 +1,16 @@
 from collections import defaultdict
+from io import TextIOWrapper
 from typing import NamedTuple
 
 
-def parse_stacks() -> dict[int, list[str]]:
+def parse_stacks(file: TextIOWrapper) -> dict[int, list[str]]:
     stacks: dict[int, list[str]] = defaultdict(list)
-    with open("input.txt", "r") as file:
-        for line in file:
-            if "[" not in line:
-                break
-            for i in range(1, len(line), 4):
-                if line[i] != " ":
-                    stacks[(i//4)+1].append(line[i])
+    for line in file:
+        if "[" not in line:
+            break
+        for i in range(1, len(line), 4):
+            if line[i] != " ":
+                stacks[(i//4)+1].append(line[i])
     for i, stack in stacks.items():
         stacks[i] = list(reversed(stack))
     return stacks
@@ -22,28 +22,25 @@ class Instruction(NamedTuple):
     dst_stack: int
 
 
-def parse_instructions() -> list[Instruction]:
+def parse_instructions(file: TextIOWrapper) -> list[Instruction]:
     import re
     regex = re.compile(r"move (\d+) from (\d+) to (\d+)")
 
     instructions: list[Instruction] = []
-    with open("input.txt", "r") as file:
-        # skip the stacks
-        while file.readline() != "\n":
-            continue
-        for line in file:
-            chunks = re.match(regex, line.strip())
-            if not chunks:
-                break
-            amount, src_stack, dst_stack = map(int, chunks.groups())
-            instructions.append(Instruction(amount, src_stack, dst_stack))
+    for line in file:
+        chunks = re.match(regex, line.strip())
+        if not chunks:
+            break
+        amount, src_stack, dst_stack = map(int, chunks.groups())
+        instructions.append(Instruction(amount, src_stack, dst_stack))
 
     return instructions
 
 
-def part_one() -> str:
-    stacks = parse_stacks()
-    instructions = parse_instructions()
+def part_one(file: TextIOWrapper) -> str:
+    stacks = parse_stacks(file)
+    file.readline()  # skip empty line
+    instructions = parse_instructions(file)
 
     for instruction in instructions:
         crates_to_move = (stacks[instruction.src_stack].pop()
@@ -54,9 +51,10 @@ def part_one() -> str:
     return "".join(stack[-1] for _, stack in sorted(stacks.items()))
 
 
-def part_two() -> str:
-    stacks = parse_stacks()
-    instructions = parse_instructions()
+def part_two(file: TextIOWrapper) -> str:
+    stacks = parse_stacks(file)
+    file.readline()  # skip empty line
+    instructions = parse_instructions(file)
 
     for instruction in instructions:
         crates_to_move = [stacks[instruction.src_stack].pop()
@@ -68,8 +66,10 @@ def part_two() -> str:
 
 
 def main() -> None:
-    print(part_one())
-    print(part_two())
+    with open("input.txt", "r") as file:
+        print(part_one(file))
+        file.seek(0)
+        print(part_two(file))
 
 
 if __name__ == "__main__":
