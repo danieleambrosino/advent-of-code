1843fc5b9b18073e1745659cf0345146ee4d8ce5
Daniele Ambrosino <daniele.ambrosino@jethr.com>
2025-12-07 17:00:21 +0100

    part two

--- 2025/04/main.py
+++ 2025/04/main.py
@@ -1,9 +1,9 @@
 import numpy
 
 
-def count_accessible_rolls(grid: numpy.ndarray) -> int:
+def get_accessible_rolls(grid: numpy.ndarray) -> list[tuple[int, int]]:
     rows, cols = grid.shape
-    count = 0
+    positions: list[tuple[int, int]] = []
     for r in range(rows):
         for c in range(cols):
             if grid[r, c] != "@":
@@ -12,7 +12,29 @@ def count_accessible_rolls(grid: numpy.ndarray) -> int:
                 max(0, r - 1) : min(rows, r + 2), max(0, c - 1) : min(cols, c + 2)
             ]
             if len(neighborhood[neighborhood == "@"]) <= 4:
-                count += 1
+                positions.append((r, c))
+    return positions
+
+
+def count_accessible_rolls(grid: numpy.ndarray) -> int:
+    return len(get_accessible_rolls(grid))
+
+
+def remove_accessible_rolls(
+    grid: numpy.ndarray,
+    accessible_rolls: list[tuple[int, int]],
+) -> None:
+    for r, c in accessible_rolls:
+        grid[r, c] = "."
+
+def remove_all_accessible_rolls(grid: numpy.ndarray) -> int:
+    count = 0
+    while True:
+        accessible_rolls = get_accessible_rolls(grid)
+        if not accessible_rolls:
+            break
+        count += len(accessible_rolls)
+        remove_accessible_rolls(grid, accessible_rolls)
     return count
 
 
@@ -20,6 +42,7 @@ def main() -> None:
     with open("input.txt") as file:
         grid = numpy.array([list(line) for line in file.read().strip().splitlines()])
     print(count_accessible_rolls(grid))
+    print(remove_all_accessible_rolls(grid))
 
 
 if __name__ == "__main__":
